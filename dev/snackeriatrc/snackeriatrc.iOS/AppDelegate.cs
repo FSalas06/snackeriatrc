e805bfb49cb9288b074d3fb923901a94667c79e4
Jose Salas <fsalas.y@gmail.com>
2021-06-28 16:11:41 -0500

    INIT SNACKERIA TRC
    INIT SNACKERIA TRC

---
+++ dev/snackeriatrc/snackeriatrc.iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿
+using Foundation;
+using UIKit;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace snackeriatrc.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        //
+        // This method is invoked when the application has loaded and is ready to run. In this 
+        // method you should instantiate the window, load the UI into it and then make the window
+        // visible.
+        //
+        // You have 17 seconds to return from this method, or iOS will terminate your application.
+        //
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+#if ENABLE_TEST_CLOUD
+            Xamarin.Calabash.Start();
+#endif
+            AppCenter.Start("1f58bee1-08e2-4ff2-9c26-0a29224fe985",
+                   typeof(Analytics), typeof(Crashes));
+
+            Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
---
+++ dev/snackeriatrc/snackeriatrc/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using snackeriatrc.Services;
+using snackeriatrc.Views;
+
+namespace snackeriatrc
+{
+    public partial class App : Application
+    {
+        public static bool UseMockDataStore = true;
+
+        public App()
+        {
+            InitializeComponent();
+
+            if (UseMockDataStore)
+            {
+                DependencyService.Register<MockDataStore>();
+            }
+            else
+            {
+                DependencyService.Register<AzureDataStore>();
+            }
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
